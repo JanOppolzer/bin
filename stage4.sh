2bb8b0cfc32bb74c0ffa9131a899a312966502b3
Jan Oppolzer <jan@oppolzer.cz>
2019-07-01 11:35:51 +0200

    stage4.sh: Set the long key version

--- stage4.sh
+++ stage4.sh
@@ -43,6 +43,6 @@ EXCLUDES="\
 sudo tar $EXCLUDES -czpf $STAGE4 /
 sha256sum $STAGE4 >> $STAGE4.sha256
 
-gpg2 -u 0x1A9D753D5E0A17BD --output $STAGE4.gpg --recipient jan@oppolzer.cz --encrypt --sign $STAGE4
+gpg2 -u 6B78BA37143C14C3A9699485DBF935FB5101AA6C --output $STAGE4.gpg --recipient jan@oppolzer.cz --encrypt --sign $STAGE4
 sha256sum $STAGE4.gpg >> $STAGE4.gpg.sha256
 
